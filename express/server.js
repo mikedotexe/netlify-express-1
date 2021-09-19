a3553e0747b400d963f2fd3420b2f59b2ea3efec
Mike Purvis <mikedotexe@gmail.com>
2021-09-19 10:30:42 -0700

    use cors specifying https://pww.mike.test for debugging

--- express/server.js
+++ express/server.js
@@ -5,7 +5,9 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+  origin: 'https://pww.mike.test'
+}));
 
 const router = express.Router();
 router.get('/', (req, res) => {
